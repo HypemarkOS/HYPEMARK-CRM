03b3020b185270ffa7ce84d71faeb10524c37f77
HypemarkOS <namani.ajay123@gmail.com>
2026-08-09 22:51:45 +0530

    Make CRM self-initialize from standalone script

--- backend/Setup.js
+++ backend/Setup.js
@@ -1,5 +1,16 @@
 function initializeCRMService_() {
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var props = PropertiesService.getScriptProperties();
+  var ss;
+  var existingId = props.getProperty('CRM_SPREADSHEET_ID');
+
+  if (existingId) {
+    ss = SpreadsheetApp.openById(existingId);
+  } else {
+    var active = SpreadsheetApp.getActiveSpreadsheet();
+    ss = active || SpreadsheetApp.create(APP.NAME + ' Data');
+    props.setProperty('CRM_SPREADSHEET_ID', ss.getId());
+  }
+
   var definitions = [
     [APP.SHEETS.CLIENTS, ['Client ID','Client Name','Business Name','Contact Person','Mobile','Email','Address','GST','Status','Created On']],
     [APP.SHEETS.ENGAGEMENTS, ['Engagement ID','Client ID','Service','Start Date','End Date','Status']],
@@ -11,11 +22,25 @@ function initializeCRMService_() {
     [APP.SHEETS.USERS, ['User ID','Name','Role','Email','Status']],
     [APP.SHEETS.SETTINGS, ['Key','Value']]
   ];
+
   definitions.forEach(function(def) {
     var sheet = ss.getSheetByName(def[0]) || ss.insertSheet(def[0]);
     if (sheet.getLastRow() === 0) sheet.getRange(1,1,1,def[1].length).setValues([def[1]]);
     sheet.getRange(1,1,1,def[1].length).setFontWeight('bold');
     sheet.setFrozenRows(1);
   });
-  return true;
+
+  ensureCRMOpenTrigger_(ss);
+  console.log('HYPEMARK CRM spreadsheet: ' + ss.getUrl());
+  return ss.getUrl();
+}
+
+function ensureCRMOpenTrigger_(spreadsheet) {
+  var triggers = ScriptApp.getProjectTriggers();
+  var exists = triggers.some(function(trigger) {
+    return trigger.getHandlerFunction() === 'onOpen';
+  });
+  if (!exists) {
+    ScriptApp.newTrigger('onOpen').forSpreadsheet(spreadsheet).onOpen().create();
+  }
 }
