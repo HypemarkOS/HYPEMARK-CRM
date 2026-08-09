a72d3b8af2154883f5b1d47fc1adfa551dfc642a
HypemarkOS <namani.ajay123@gmail.com>
2026-08-09 23:47:58 +0530

    Add payments page route

--- Code.js
+++ Code.js
@@ -2,7 +2,7 @@
  * HYPEMARK CRM v1
  * Public Apps Script entry points.
  */
-function doGet(e) { var page=e&&e.parameter?e.parameter.page:''; var file=page==='add'?'AddClient':'Index'; return HtmlService.createTemplateFromFile(file).evaluate().setTitle(page==='add'?'Add Client | HYPEMARK CRM':'HYPEMARK CRM').setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL); }
+function doGet(e) { var page=e&&e.parameter?e.parameter.page:''; var file=page==='add'?'AddClient':page==='payments'?'Payments':'Index'; var title=page==='add'?'Add Client | HYPEMARK CRM':page==='payments'?'Payments | HYPEMARK CRM':'HYPEMARK CRM'; return HtmlService.createTemplateFromFile(file).evaluate().setTitle(title).setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL); }
 function include(fileName){return HtmlService.createHtmlOutputFromFile(fileName).getContent();}
 function onOpen(){SpreadsheetApp.getUi().createMenu('🚀 HYPEMARK CRM').addItem('➕ Add Client','showAddClient').addSeparator().addItem('⚙ Initialize CRM','initializeCRM').addToUi();}
 function showAddClient(){var html=HtmlService.createTemplateFromFile('AddClient').evaluate().setWidth(900).setHeight(700);SpreadsheetApp.getUi().showModalDialog(html,'Add New Client');}
