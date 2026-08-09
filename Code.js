a9c7b9e325d45abe53092d66b1047c0d443c5cb4
HypemarkOS <namani.ajay123@gmail.com>
2026-08-09 23:08:36 +0530

    Build client management UI entrypoints

--- Code.js
+++ Code.js
@@ -3,7 +3,14 @@
  * Public Apps Script entry points.
  */
 function doGet() {
-  return HtmlService.createHtmlOutput('<h2>HYPEMARK CRM</h2><p>CRM is ready.</p>').setTitle('HYPEMARK CRM');
+  return HtmlService.createTemplateFromFile('Index')
+    .evaluate()
+    .setTitle('HYPEMARK CRM')
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+}
+
+function include(fileName) {
+  return HtmlService.createHtmlOutputFromFile(fileName).getContent();
 }
 
 function onOpen() {
@@ -15,7 +22,8 @@ function onOpen() {
 }
 
 function showAddClient() {
-  var html = HtmlService.createHtmlOutputFromFile('AddClient')
+  var html = HtmlService.createTemplateFromFile('AddClient')
+    .evaluate()
     .setWidth(900)
     .setHeight(700);
   SpreadsheetApp.getUi().showModalDialog(html, 'Add New Client');
@@ -24,3 +32,5 @@ function showAddClient() {
 function initializeCRM() { return initializeCRMService_(); }
 function createClient(data) { return createClientService_(data); }
 function getClients() { return getClientsService_(); }
+function getClient(clientId) { return getClientService_(clientId); }
+function updateClient(clientId, data) { return updateClientService_(clientId, data); }
