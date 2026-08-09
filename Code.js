e76ca849d2eb410dd20c20f98625ad1649ccff75
HypemarkOS <namani.ajay123@gmail.com>
2026-08-09 22:52:02 +0530

    Route initialization through CRM service

--- Code.js
+++ Code.js
@@ -26,30 +26,7 @@ function showAddClient() {
 }
 
 function initializeCRM() {
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var definitions = [
-    [APP.SHEETS.CLIENTS, ['Client ID','Client Name','Business Name','Contact Person','Mobile','Email','Address','GST','Status','Created On']],
-    [APP.SHEETS.ENGAGEMENTS, ['Engagement ID','Client ID','Service','Start Date','End Date','Status']],
-    [APP.SHEETS.PROJECTS, ['Project ID','Client ID','Engagement ID','Project Name','Status','Created On']],
-    [APP.SHEETS.DELIVERABLES, ['Deliverable ID','Project ID','Type','Quantity','Status']],
-    [APP.SHEETS.CONTENT_BANK, ['Content ID','Deliverable ID','Title','Status','Assigned To','Publish Date']],
-    [APP.SHEETS.ACTIVITIES, ['Activity ID','Entity','Entity ID','Action','User','Date Time']],
-    [APP.SHEETS.PAYMENTS, ['Payment ID','Client ID','Amount','Payment Date','Status']],
-    [APP.SHEETS.USERS, ['User ID','Name','Role','Email','Status']],
-    [APP.SHEETS.SETTINGS, ['Key','Value']]
-  ];
-
-  definitions.forEach(function(definition) {
-    var sheet = ss.getSheetByName(definition[0]) || ss.insertSheet(definition[0]);
-    if (sheet.getLastRow() === 0) {
-      sheet.getRange(1, 1, 1, definition[1].length).setValues([definition[1]]);
-    }
-    sheet.getRange(1, 1, 1, definition[1].length).setFontWeight('bold');
-    sheet.setFrozenRows(1);
-  });
-
-  SpreadsheetApp.getUi().alert('HYPEMARK CRM initialized successfully.');
-  return true;
+  return initializeCRMService_();
 }
 
 function createClient(data) {
