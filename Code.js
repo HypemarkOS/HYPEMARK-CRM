5cdec2f3bd90a720ef984fd3627dcbb0b9995f44
HypemarkOS <namani.ajay123@gmail.com>
2026-08-11 00:44:14 +0530

    Wire ProjectsPro into actual App router

--- Code.js
+++ Code.js
@@ -12,10 +12,12 @@ function doGet(e) {
     var premiumShell = HtmlService.createHtmlOutputFromFile('PremiumShell').getContent();
     var logoFix = HtmlService.createHtmlOutputFromFile('LogoFix').getContent();
     var clientsPro = HtmlService.createHtmlOutputFromFile('ClientsPro').getContent();
+    var projectsPro = HtmlService.createHtmlOutputFromFile('ProjectsPro').getContent();
     var logoData = getHypeMarkLogoDataUri_();
     logoFix = logoFix.replace('__HYPEMARK_LOGO__', logoData);
     var html = output.getContent();
     html = html.replace('</head>', theme + brandFix + premiumShell + logoFix + clientsPro + '</head>');
+    html = html.replace('</body>', projectsPro + '</body>');
     output = HtmlService.createHtmlOutput(html);
   }
   return output.setTitle(auth.authenticated ? 'HYPEMARK CRM' : 'Sign in | HYPEMARK CRM')
